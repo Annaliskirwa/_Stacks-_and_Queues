e6f5028d523754270d2750714609212a50cb505c
Annalis Kirwa <39658946+Annaliskirwa@users.noreply.github.com>
2022-06-23 00:04:48 +0300

    Guess game

---
+++ BinarySearch/GuessNumber.java
@@ -0,0 +1,41 @@
+/**
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+ 
+
+*/
+
+//My first approach, poor time complexity though. Tranverse the entire numbers to n checking if the nth number is the guessed number. if guessed, return the number.
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        for (int i = 1; i < n; i++){
+            if (guess(i) == 0){
+                return i;
+            }
+            
+        }
+        return n;
+        
+    }
+}
